611b538bca5680647ca667df195e6f6c8499b29f
bichanna <nobu.bichanna@gmail.com>
2024-05-04 19:22:08 -0400

    Create lexer and parser

---
+++ Lexer.m
@@ -0,0 +1,147 @@
+classdef Lexer < handle
+    properties
+        source
+        index
+        current
+        tokens
+    end
+
+    methods
+        function lexer = Lexer(source)
+            lexer.source = source{1};
+            lexer.index = 0;
+            lexer.current = ' ';
+            lexer.tokens = [];
+        end
+
+        function tokenize(lexer)
+            currentChar = lexer.advance;
+
+            while ~lexer.isEnd
+                % fprintf("Current: %s\n", lexer.current);
+                switch currentChar
+                    case ' '
+                    case '\t'
+                    case '\n'
+                        1;
+                    case '('
+                        token = lexer.createNoValueToken("(");
+                        lexer.appendToken(token);
+                    case ')'
+                        token = lexer.createNoValueToken(")");
+                        lexer.appendToken(token);
+                    case '+'
+                        token = lexer.createNoValueToken("+");
+                        lexer.appendToken(token);
+                    case '-'
+                        token = lexer.createNoValueToken("-");
+                        lexer.appendToken(token);
+                    case '*'
+                        token = lexer.createNoValueToken("*");
+                        lexer.appendToken(token);
+                    case '/'
+                        token = lexer.createNoValueToken("/");
+                        lexer.appendToken(token);
+                    case '^'
+                        token = lexer.createNoValueToken("^");
+                        lexer.appendToken(token);
+                    case '.'
+                        token = lexer.createNoValueToken(".");
+                        lexer.appendToken(token);
+                    otherwise
+                        if lexer.isNumber(currentChar)
+                            token = lexer.tokenizeNumber();
+                            lexer.appendToken(token);
+                        else
+                            lexer.index = -1;
+                            fprintf("Invalid token: '%s'\n", currentChar);
+                            break;
+                        end
+                end
+
+                currentChar = lexer.advance;
+                % fprintf("After advance: %s\n", lexer.current);
+            end
+            
+            endToken = lexer.createNoValueToken("EOF");
+            lexer.appendToken(endToken);
+        end
+
+        function newToken = tokenizeNumber(lexer)
+            number = "";
+
+            while lexer.isNumber(lexer.current)
+                number = append(number, lexer.current);
+                lexer.advance();
+            end
+
+            nextChar = lexer.nextChar;
+            if all(lexer.current == '.') && all(lexer.isNumber(nextChar))
+                number = append(number, lexer.current);
+                lexer.advance();
+
+                while lexer.isNumber(lexer.current)
+                    number = append(number, lexer.current);
+                    lexer.advance();
+                end
+            end
+            
+            % fprintf("Before: %s\n", lexer.current);
+            lexer.revert();
+            % fprintf("After: %s\n", lexer.current);
+
+            num = str2double(number);
+            newToken = lexer.createToken("number", num);
+        end
+
+        function result = nextChar(lexer)
+            if lexer.isNextEnd()
+                result = '\0';
+            else
+                result = lexer.source(lexer.index+1);
+            end
+        end
+
+        function result = isNumber(~, char)
+            result = isstrprop(char, "digit");
+        end
+
+        function newToken = createToken(~, kind, value)
+            newToken.kind = kind;
+            newToken.value = value;
+        end
+
+        function newToken = createNoValueToken(~, kind)
+            newToken.kind = kind;
+            newToken.value = [];
+        end
+
+        function appendToken(lexer, token)
+            lexer.tokens = [lexer.tokens, token];
+        end
+
+        function revert(lexer)
+            lexer.index = lexer.index - 1;
+            lexer.current = lexer.source(lexer.index);
+        end
+
+        function currentChar = advance(lexer)
+            lexer.index = lexer.index + 1;
+            if ~lexer.isEnd
+                lexer.current = lexer.source(lexer.index);
+            else
+                lexer.current = '\0';
+            end
+
+            currentChar = lexer.current;
+        end
+
+        function isNextEnd = isNextEnd(lexer)
+            isNextEnd = lexer.index >= length(lexer.source) || lexer.index == -1;
+        end
+
+        function isEnd = isEnd(lexer)
+            isEnd = lexer.index - 1 >= length(lexer.source) || lexer.index == -1;
+        end
+    end
+end
---
+++ Parser.m
@@ -0,0 +1,185 @@
+classdef Parser < handle
+    properties
+        tokens
+        index
+        current
+        ast
+    end
+
+    methods
+        function parser = Parser(tokens)
+            parser.tokens = tokens;
+            parser.index = 0;
+            parser.current = struct("kind", "start", "value", []);
+            parser.ast = [];
+        end
+
+        function parse(parser)
+            parser.advance;
+            
+            while ~parser.isEnd
+                node = parser.parseStmt;
+
+                if node.kind ~= "error"
+                    parser.appendNode(node);
+                else
+                    fprintf("Error: %s\n", node.left);
+                    break;
+                end
+            end
+        end
+
+        function node = parseStmt(parser)
+            node = parser.parseExpr;
+            
+            if node.kind == "error"
+                return;
+            elseif parser.current.kind ~= '.'
+                node = parser.createError("expected '.'");
+            else
+                parser.advance;
+            end
+        end
+
+        function node = parseExpr(parser)
+            node = parser.parseTerm;
+        end
+
+        function node = parseTerm(parser)
+            node = parser.parseFactor;
+
+            if node.kind == "error"
+                return;
+            elseif parser.current.kind == '+'
+                parser.advance;
+                right = parser.parseFactor;
+                if right.kind == "error"
+                    node = right;
+                else
+                    node = parser.createBinaryNode("add", node, right);
+                end
+            elseif parser.current.kind == "-"
+                parser.advance;
+                right = parser.parseFactor;
+                if right.kind == "error"
+                    node = right;
+                else
+                    node = parser.createBinaryNode("sub", node, right);
+                end
+            end
+        end
+
+        function node = parseFactor(parser)
+            node = parser.parseUnary;
+
+            if node.kind == "error"
+                return;
+            elseif parser.current.kind == '/'
+                parser.advance;
+                right = parser.parseUnary;
+                if right.kind == "error"
+                    node = right;
+                else
+                    node = parser.createBinaryNode("div", node, right);
+                end
+            elseif parser.current.kind == '*'
+                parser.advance;
+                right = parser.parseUnary;
+                if right.kind == "error"
+                    node = right;
+                else
+                    node = parser.createBinaryNode("mul", node, right);
+                end
+            end
+        end
+
+        function node = parseUnary(parser)
+            if parser.current.kind == '-'
+                parser.advance;
+                value = parser.parseUnary;
+                if value.kind == "error"
+                    node = value;
+                else
+                    node = parser.createNegateNode("numNeg", value);
+                end
+            else
+                node = parser.parsePrimary;
+            end
+        end
+
+        function node = parsePrimary(parser)
+            switch parser.current.kind
+                case "number"
+                    node = parser.createLiteralNode("number", parser.current.value);
+                    parser.advance;
+                case '('
+                    parser.advance;
+                    expr = parser.parseExpr;
+                    if expr.kind == "error"
+                        node = expr;
+                    else
+                        node = parser.createGroupNode(expr);
+                        if parser.current.kind == ')'
+                            parser.advance;
+                        else
+                            node = parser.createError("expected ')'");
+                        end
+                    end
+                otherwise
+                    node = parser.createError("invalid token");
+            end
+        end
+
+        function currentToken = advance(parser)
+            if ~parser.isEnd()
+                parser.index = parser.index + 1;
+                parser.current = parser.tokens(parser.index);
+            end
+
+            currentToken = parser.current;
+        end
+
+        function result = isEnd(parser)
+            result = parser.current.kind == "EOF";
+        end
+
+        function appendNode(parser, node)
+            parser.ast = [parser.ast, node];
+        end
+
+        function err = createError(~, msg)
+            err.kind = "error";
+            err.operator = [];
+            err.left = msg;
+            err.right = [];
+        end
+
+        function node = createNegateNode(~, operator, value)
+            node.kind = "unary";
+            node.operator = operator;
+            node.left = value;
+            node.right = [];
+        end
+
+        function node = createBinaryNode(~, operator, left, right)
+            node.kind = "binary";
+            node.operator = operator;
+            node.left = left;
+            node.right = right;
+        end
+
+        function node = createGroupNode(~, value)
+            node.kind = "group";
+            node.operator = [];
+            node.left = value;
+            node.right = [];
+        end
+
+        function node = createLiteralNode(~, kind, value)
+            node.kind = kind;
+            node.operator = [];
+            node.left = value;
+            node.right = [];
+        end
+    end
+end
